4fabc2785c20cb6a54885aa1edf17246d97820b7
phd <oukitel8100q@gmail.com>
2021-12-18 21:39:53 +0200

    Change code lab05 and add report

--- lab05/src/main.c
+++ lab05/src/main.c
@@ -1,10 +1,12 @@
+
 int main() {
- int factorial = 1;//result 5!=1*2*3*4*5
- int n = 10;// number 12321!
+ int factorial = 1 ;//result 5!=1*2*3*4*5
+ int n = 5;// number 12321!
    for(int i = 1; i <= n; i++) {
         factorial *= i;
     }
-
+ int res;//answer for qes 10*n!
+	res = 10*factorial;
 
 return 0;
 }
